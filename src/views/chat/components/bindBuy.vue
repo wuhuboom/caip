c694922e97df0c78cd2679ec851291b415997dc1
fafafa@gmail.com <fafafa@gmail.com>
2025-01-05 21:40:04 +0700

    fix

---
+++ src/views/chat/components/bindBuy.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="order-detail m-t-4" @click.stop="goDetail">
+    <p class="align-center">
+      <img class="d-img m-r-4 ord" src="@/assets/img/ord.png" alt="" />
+      #{{ doc.data.lottery }}
+    </p>
+    <div class="detail m-l-8 m-t-8 flex-column">
+      <dl class="oder-desc d-flex justify-between p-l-8 p-r-8">
+        <dd class="align-center">
+          <img class="d-img m-r-4 fire" src="@/assets/img/fire.png" alt="" />
+          {{ doc.data.user }}#发起合买
+        </dd>
+        <dd class="money">￥{{ divide(doc.data.money) }}</dd>
+      </dl>
+      <dl class="justify-between p-l-8 p-r-8">
+        <dd>
+          <p class="rate align-center">
+            <span class="rate-num">{{ `${doc.data.bingos * 100}%` }}</span>
+          </p>
+        </dd>
+        <dd>
+          <p class="now-btn center-center">立即跟</p>
+        </dd>
+      </dl>
+    </div>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line no-unused-vars
+import userApi from "@/api/user";
+export default {
+  name: "PopupMoney",
+  data() {
+    return {};
+  },
+  props: {
+    doc: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  methods: {
+    goDetail() {
+      //window.open(`#/order/detail/${this.doc.data.id}`);
+      this.$router.push(`/order/detail/${this.doc.data.id}`);
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+.order-detail {
+  cursor: pointer;
+  min-width: 250px;
+  height: 120px;
+  background: #ffffff;
+  border-radius: 5px 5px 5px 5px;
+  border: 1px solid #f0f0f0;
+  padding: 14px 10px 14px;
+  .ord {
+    width: 18px;
+    height: 18px;
+  }
+  .fire {
+    width: 17px;
+    height: 17px;
+  }
+  .detail {
+    color: #fff;
+    height: 70px;
+    background: #00a752;
+    border-radius: 5px 5px 5px 5px;
+
+    justify-content: space-around;
+  }
+  .oder-desc {
+    align-items: flex-end;
+    font-size: 14px;
+  }
+  .money {
+    font-size: 18px;
+  }
+  .rate {
+    background: url("@/assets/img/rates.png") no-repeat center center;
+    background-size: 100% 100%;
+    width: 54px;
+    height: 12px;
+    font-size: 8px;
+    color: #bf2935;
+    padding-bottom: 2px;
+    .rate-num {
+      min-width: 28px;
+      text-align: center;
+    }
+  }
+  .now-btn {
+    color: #03bf5f;
+    width: 54px;
+    height: 18px;
+    background: #ffffff;
+    border-radius: 5px 5px 5px 5px;
+    font-size: 10px;
+  }
+}
+</style>
---
+++ src/views/chat/components/redImg.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="m-t-4 red-img">
+    <img class="d-img pointer" :src="red1" alt="" @click="open" />
+    <van-popup class="popupOpen pointer" v-model="showOpen">
+      <ul class="flex-column center-center font14">
+        <li class="align-center font14">
+          <img
+            class="d-img user-red m-r-8"
+            :src="
+              doc.img
+                ? doc.img.includes('http')
+                  ? doc.img
+                  : `${$baseURL}/${doc.img}`
+                : userPic
+            "
+          />
+          收到{{ doc.user }}的红包
+        </li>
+        <li class="d-desc m-t-12">恭喜发财, 大吉大利</li>
+      </ul>
+    </van-popup>
+    <van-popup class="popupFinish" v-model="showFinish">
+      <ul class="flex-column center-center font14 m-b-8">
+        <li class="align-center font14 names">
+          <img
+            class="d-img user-red m-r-8"
+            :src="
+              doc.img
+                ? doc.img.includes('http')
+                  ? doc.img
+                  : `${$baseURL}/${doc.img}`
+                : userPic
+            "
+          />
+          收到{{ doc.user }}的红包
+        </li>
+        <li class="d-desc m-b-8">恭喜发财, 大吉大利</li>
+        <li class="d-meng d-flex m-b-8">
+          1212.00
+          <span>元</span>
+        </li>
+        <li class="view pointer">
+          全部领取记录<van-icon :size="12" name="arrow" />
+        </li>
+      </ul>
+    </van-popup>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line no-unused-vars
+import userApi from "@/api/user";
+import red1 from "@/assets/img/red1.png";
+import red2 from "@/assets/img/red2.png";
+export default {
+  name: "RedImg",
+  data() {
+    return {
+      red1,
+      red2,
+      showOpen: false,
+      showFinish: false,
+    };
+  },
+  props: {
+    doc: {
+      type: Object,
+      default: () => {},
+    },
+    userPic: {
+      type: String,
+      default: "",
+    },
+  },
+  methods: {
+    open() {
+      this.showFinish = true;
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+.red-img {
+  width: 234px;
+  height: 86px;
+  img {
+    object-fit: contain;
+  }
+}
+.popupOpen {
+  padding-top: 73px;
+  width: 255px;
+  height: 397px;
+  background: url("@/assets/img/openRed.png") no-repeat center;
+  background-size: 100% 100%;
+  color: #feedaf;
+
+  .d-desc {
+    font-size: 18px;
+  }
+}
+.user-red {
+  width: 16px;
+  height: 16px;
+}
+.popupFinish {
+  padding-top: 204px;
+  width: 255px;
+  height: 399px;
+  background: url("@/assets/img/finishRed.png") no-repeat center;
+  background-size: 100% 100%;
+  .names {
+    color: #191919;
+  }
+  .d-desc {
+    color: #999999;
+    font-size: 12px;
+  }
+  .d-meng {
+    font-size: 42px;
+    color: #cdac74;
+    align-items: baseline;
+    span {
+      font-size: 12px;
+    }
+  }
+  .view {
+    color: #b78756;
+    font-size: 12px;
+  }
+}
+</style>
