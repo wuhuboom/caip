4df9a7e4a12445980b74ca7de2c47cb9a48f7b6c
fafafa@gmail.com <fafafa@gmail.com>
2025-01-07 13:59:44 +0700

    fux

--- src/views/account/team/Team3.vue
+++ src/views/account/team/Team3.vue
@@ -1,12 +1,40 @@
 <template>
   <div class="recharge-main m-t-0 p-t-0">
     <div class="recharge-title align-center">
-      <span class="no-shrink">方案编号：</span>
+      <span class="no-shrink">用户名：</span>
       <input v-model.trim="params.name" class="fund-input m-r-16" />
-      <span class="no-shrink">方案期号：</span>
-      <input v-model.trim="params.name" class="fund-input m-r-16" />
-      <span class="no-shrink">客户名称：</span>
-      <input v-model.trim="params.name" class="fund-input m-r-16" />
+      <span class="no-shrink">充值/提现时间：</span>
+      <el-date-picker
+        style="width: 310px"
+        v-model="date"
+        class="g-el-input__inner m-r-16"
+        type="daterange"
+        size="small"
+        value-format="yyyy-MM-dd"
+        range-separator="至"
+        start-placeholder="开始日期"
+        end-placeholder="结束日期"
+        popper-class="g-table-popper-class"
+      >
+      </el-date-picker>
+      <span class="no-shrink">状态：</span>
+      <el-select
+        style="width: 110px"
+        class="g-el-cascader"
+        size="small"
+        v-model="params.time"
+        clearable
+        placeholder="请选择"
+        popper-class="g-el-cascader-popper"
+      >
+        <el-option
+          v-for="item in tabSimpleList"
+          :key="item.id"
+          :label="item.text"
+          :value="item.id"
+        >
+        </el-option>
+      </el-select>
       <div
         class="cp-button-main activity-search-btn"
         v-loading="loading"
@@ -17,14 +45,11 @@
     </div>
     <div class="recharge-table-container p-t-0">
       <el-table class="g-el-table" border :data="tableData.results">
-        <el-table-column prop="orderNo" label="方案号"> </el-table-column>
-        <el-table-column prop="orderNo" label="发单人"> </el-table-column>
+        <el-table-column prop="orderNo" label="客观名称"> </el-table-column>
+        <el-table-column prop="orderNo" label="充值金额"> </el-table-column>
         <el-table-column prop="orderNo" label="用户"> </el-table-column>
-        <el-table-column prop="orderNo" label="认购金额"> </el-table-column>
-        <el-table-column prop="orderNo" label="所占百分比"> </el-table-column>
-        <el-table-column prop="orderNo" label="认购时间"> </el-table-column>
-        <el-table-column prop="orderNo" label="中奖金额"> </el-table-column>
-        <el-table-column prop="orderNo" label="状态"> </el-table-column>
+        <el-table-column prop="orderNo" label="佣金"> </el-table-column>
+        <el-table-column prop="orderNo" label="充值时间"> </el-table-column>
       </el-table>
       <div class="cp-pagination-main" style="margin-top: 40px">
         <el-pagination
@@ -43,10 +68,37 @@
 
 <script>
 import userApi from "@/api/user";
+import i18n from "@/locale";
 export default {
   name: "AccountCenter",
   data() {
     return {
+      tabSimpleList: [
+        {
+          text: i18n.t("dropdown.billing.all.title.text"),
+          id: "",
+        },
+        {
+          text: i18n.t("match.records.today.text"),
+          id: 1,
+        },
+        {
+          text: i18n.t("match.records.yes.text"),
+          id: 2,
+        },
+        {
+          text: i18n.t("match.records.nearly7.text"),
+          id: 3,
+        },
+        {
+          text: i18n.t("match.records.nearly10.text"),
+          id: 4,
+        },
+        {
+          text: i18n.t("match.records.nearly30.text"),
+          id: 5,
+        },
+      ],
       tableData: {
         totalCount: 0,
         results: [],
