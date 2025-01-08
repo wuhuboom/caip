d1e6edbdd74968c477dcbd3a83ee3cc773fcc0a0
fafafa@gmail.com <fafafa@gmail.com>
2025-01-08 11:26:46 +0700

    fix

--- src/views/account/components/MemberManage.vue
+++ src/views/account/components/MemberManage.vue
@@ -1,132 +1,45 @@
 <template>
-  <div class="recharge-main">
-    <div style="margin: 0px 15px">
-      <div>
-        <span>官网地址：</span
-        ><span
-          style="margin-left: 5px"
-          v-clipboard:copy="textToCopy(headData.website)"
-          v-clipboard:success="onCopySuccess"
-        >
-          {{ headData.website }}
-        </span>
-        <i
-          class="el-icon-document-copy m-l-8"
-          v-clipboard:copy="textToCopy(headData.website)"
-          v-clipboard:success="onCopySuccess"
-        ></i>
-      </div>
-      <div>
-        <div>
-          <span>推广链接：</span
-          ><span
-            style="margin-left: 5px"
+  <div class="recharge-main center-center">
+    <div class="m-l-12 font16 p-t-16 m-r-12 p-b-16 p-l-32 main-cont">
+      <ul class="m-b-32 align-center">
+        <li class="m-r-24">推广码</li>
+        <li class="align-center">
+          {{ user.invitationCode }}
+          <span
+            class="m-l-8 pointer"
+            v-clipboard:copy="textToCopy(`${user.invitationCode}`)"
+            v-clipboard:success="onCopySuccess"
+          >
+            <img class="d-img" src="@/assets/img/copy.png" alt="" />
+          </span>
+        </li>
+      </ul>
+      <ul class="m-b-32 align-center">
+        <li class="m-r-24">推广链接</li>
+        <li class="align-center">
+          <p>{{ myLink }}</p>
+          <span
+            class="m-l-8 pointer"
             v-clipboard:copy="textToCopy(myLink)"
             v-clipboard:success="onCopySuccess"
           >
-            {{ myLink }}
+            <img class="d-img" src="@/assets/img/copy.png" alt="" />
           </span>
-          <i
-            class="el-icon-document-copy m-l-8"
-            v-clipboard:copy="textToCopy(myLink)"
+        </li>
+      </ul>
+      <ul class="align-center">
+        <li class="m-r-24">官方地址</li>
+        <li class="align-center">
+          <p class="align-center">{{ headData.website }}</p>
+          <span
+            class="m-l-8 pointer"
+            v-clipboard:copy="textToCopy(headData.website)"
             v-clipboard:success="onCopySuccess"
-          ></i>
-        </div>
-        <div>
-          <span>邀请码：</span
-          ><span style="margin-left: 5px">{{ user.invitationCode }}</span>
-          <i
-            class="el-icon-document-copy m-l-8"
-            v-clipboard:copy="textToCopy(`${user.invitationCode}`)"
-            v-clipboard:success="onCopySuccess"
-          ></i>
-        </div>
-      </div>
-    </div>
-    <br />
-    <br />
-    <div class="recharge-title" style="margin: 5px 15px">
-      <span>代理说明：</span
-      ><span style="margin-left: 5px"
-        >会员注册默认为客户代理，客户代理将获得名下注册会员
-        {{ headData.point }}
-        %的中奖金额返点。</span
-      >
-    </div>
-    <div class="recharge-title align-center">
-      <input
-        v-model.trim="query.nickname"
-        class="activity-member-input center-center p-x-4 m-r-12"
-        style="height: 30px"
-        placeholder="会员昵称"
-      />
-      <el-date-picker
-        v-model="date"
-        class="g-el-input__inner"
-        type="daterange"
-        size="small"
-        value-format="yyyy-MM-dd"
-        range-separator="至"
-        start-placeholder="开始日期"
-        end-placeholder="结束日期"
-        popper-class="g-table-popper-class"
-      >
-      </el-date-picker>
-      <div
-        class="cp-button-main activity-search-btn"
-        @click="handleCurrentChange(1)"
-      >
-        查询
-      </div>
-    </div>
-    <div class="my-sum">
-      <div class="my-sum-item">
-        本月返点：
-        {{ divide(headData.month) }}
-        元
-      </div>
-      <div class="my-sum-item">
-        上月返点：{{ divide(headData.lastMonth) }} 元
-      </div>
-    </div>
-    <div class="recharge-table-container" style="background-color: transparent">
-      <table rules="all" align="center" class="recharge-table">
-        <tr align="center" class="recharge-table-header">
-          <th height="40" width="120">会员昵称</th>
-          <th height="40" width="100">今日中奖</th>
-          <th height="40" width="100">本月消费</th>
-          <th height="40" width="100">本月中奖</th>
-          <th height="40" width="155">最后投注</th>
-          <th height="40" width="155">最后登录</th>
-          <th height="40" width="155">注册时间</th>
-        </tr>
-        <tr
-          align="center"
-          class="recharge-table-header"
-          v-for="(item, index) in tableData.results"
-          :key="index"
-        >
-          <td height="40">{{ item.nickname }}</td>
-          <td height="40">{{ divide(item.todayWin) }}</td>
-
-          <td height="40">{{ divide(item.monthConsume) }}</td>
-          <td height="40">{{ divide(item.monthWin) }}</td>
-          <td height="40">{{ $dayjsTime(item.lastBet) }}</td>
-          <td height="40">{{ $dayjsTime(item.theNewLoginTime) }}</td>
-          <td height="40">{{ $dayjsTime(item.createdAt) }}</td>
-        </tr>
-      </table>
-    </div>
-    <div class="p-b-32 center-center">
-      <el-pagination
-        class="g-el-pagination"
-        @current-change="handleCurrentChange"
-        :current-page="query.pageNo"
-        :page-size="query.pageSize"
-        layout="total, prev, pager, next"
-        :total="tableData.totalCount"
-      >
-      </el-pagination>
+          >
+            <img class="d-img" src="@/assets/img/copy.png" alt="" />
+          </span>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -198,4 +111,32 @@ export default {
   },
 };
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.main-cont {
+  background: #623525;
+  height: 600px;
+  width: 100%;
+  font-size: 16px;
+  & > ul {
+    & > li:first-child {
+      width: 66px;
+      text-align: right;
+    }
+    i {
+      color: #b78756;
+      font-size: 18px;
+    }
+    p {
+      height: 30px;
+      line-height: 30px;
+      background: #fdf1bd;
+      padding: 0 8px;
+      color: #000;
+    }
+    .d-img {
+      width: 24px;
+      height: 24px;
+    }
+  }
+}
+</style>
