5e7260d33cc7b65b7c078e7ebd9ec2c591c37ed9
yehceng <leahkirby574@gmail.com>
2025-02-28 16:45:38 +0800

    fixz

--- src/views/game/components/ball21.vue
+++ src/views/game/components/ball21.vue
@@ -13,9 +13,9 @@
 </template>
 
 <script>
-import Rowball from "./Rowball.vue";
-import bets from "@/plugins/bets";
 import tipsDialog from "@/components/tipsDialog";
+import bets from "@/plugins/bets";
+import Rowball from "./Rowball.vue";
 export default {
   name: "InfoMain",
   data() {
@@ -32,9 +32,9 @@ export default {
   watch: {
     nums: {
       handler() {
-        const status = bets.chose5(this.nums);
+        const status = bets.chose16(this.nums);
         if (typeof status !== "number") {
-          this.$emit("total", status.totalBets || 0);
+          this.$emit("total", 0);
           return;
         }
         this.$emit("total", 1, this.nums);
@@ -63,7 +63,7 @@ export default {
       this.$refs.$Rowball2.clear();
     },
     add() {
-      const status = bets.chose5(this.nums);
+      const status = bets.chose16(this.nums);
       console.log(status);
       if (typeof status !== "number") {
         this.$refs.$tipsDialog.open(status.err);
